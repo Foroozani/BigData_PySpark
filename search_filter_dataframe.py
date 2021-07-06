b5406bf57f983d245b0f8407b862d5e996b9b0cd
Najmeh Foroozani <46888580+Foroozani@users.noreply.github.com>
2021-07-06 17:13:21 +0200

    Add files via upload

---
+++ search_filter_dataframe.py
@@ -0,0 +1,43 @@
+import pyspark
+from pyspark.sql import SparkSession
+# May take awhile locally
+spark = SparkSession.builder.appName("FunctionsHW").getOrCreate()
+
+cores = spark._jsc.sc().getExecutorMemoryStatus().keySet().size()
+print("You are working with", cores, "core(s)")
+spark
+
+fifa = spark.read.csv('Datasets/fifa19.csv',inferSchema=True,header=True)
+
+print(fifa.printSchema())
+
+from pyspark.sql.functions import *
+fifa.select(['Name','Position','Release Clause']).show(5,False)
+# Display the same results from above sorted by the players names
+fifa.select(['Name','Position']).orderBy('Name').show(5)
+fifa.select(['Name','Position','Age']).orderBy(fifa['Age'].desc()).show(5)
+
+# Select only the players who belong to a club begining with FC
+# One way
+fifa.select("Name","Club").where(fifa.Club.like("FC%")).show(5, False)
+
+# Another way
+fifa.select("Name","Club").where(fifa.Club.startswith("FC")).limit(4).toPandas()
+
+## ======================================================
+# to create a new dataframe
+df = fifa.limit(100)
+df.count()
+
+# if we slice the colomns
+df2_col = fifa.columns[0:5]
+df2 = fifa.select(df2_col)
+df2.count()
+df2.show(5,False)
+# count the colomn 
+len(df2.columns)
+# =[=======================================================
+# Filtering data
+# ========================================================
+
+fifa.filter("Age>40").select(['Name','Age']).limit(4).toPandas()
