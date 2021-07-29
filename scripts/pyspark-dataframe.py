34ae429f3a9707a565254ff786054bdf5b2107a1
Najmeh Foroozani <najmeh.foroozani@gmail.com>
2021-07-29 14:59:51 +0200

    Update pyspark-dataframe.py

--- scripts/pyspark-dataframe.py
+++ scripts/pyspark-dataframe.py
@@ -4,6 +4,8 @@ from pyspark.sql.functions import col, lit
 from pyspark.sql.types import StructType, StructField, StringType,IntegerType
 
 spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
+cores = spark._jsc.sc().getExecutorMemoryStatus().keySet().size()
+print("You are working with", cores, "core(s)")
 
 print(spark)
 
