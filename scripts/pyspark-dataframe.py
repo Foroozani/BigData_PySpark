5c5f30e53a36f63145a0ab624aa63d3bc75c8a32
Najmeh Foroozani <najmeh.foroozani@gmail.com>
2021-08-13 10:37:42 +0200

    Update pyspark-dataframe.py

--- scripts/pyspark-dataframe.py
+++ scripts/pyspark-dataframe.py
@@ -3,6 +3,9 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, lit
 from pyspark.sql.types import StructType, StructField, StringType,IntegerType
 
+# PySpark applications start with initializing SparkSession which is the entry point of PySpark as below. 
+#In case of running it in PySpark shell via pyspark executable, the shell automatically creates the session in the variable spark for users
+
 spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
 cores = spark._jsc.sc().getExecutorMemoryStatus().keySet().size()
 print("You are working with", cores, "core(s)")
